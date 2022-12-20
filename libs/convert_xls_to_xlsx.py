b272abc3a5b5bf401c21ee9b0814dc7d3bb89d0f
reiji-9720 <shino327gh@gmail.com>
2022-12-21 06:32:23 +0900

    関数処理でxlwingsを用いたコードに変更。※呼び出しても変化なし

--- libs/convert_xls_to_xlsx.py
+++ libs/convert_xls_to_xlsx.py
@@ -1,5 +1,12 @@
 import pyexcel as p
 import glob
+import os
+import openpyxl
+import xlwt
+
+import subprocess
+
+import xlwings
 
 
 ## カレントディレクトリ内での拡張子変更
@@ -9,3 +16,31 @@ def convert_type_current():
         xlsx = "{}".format(xls) + "x"
         print(xlsx)
         p.save_book_as(file_name='{}'.format(xls), dest_file_name='{}'.format(xlsx))
+
+
+def simple_check():
+    i = 0
+    for pathname,dirname, filenames in os.walk("./"):
+        for filename in filenames:
+            if filename.endswith('.xls'): # フィルタ処理 csvのみ処理
+
+                app = xlwings.App() # appの開始
+                filename = os.path.join(*pathname,filename) # ファイルの相対パス取得
+                filename_only = os.path.splitext(filename)[0].strip('/') # ファイル名拡張子なし
+                # wb = app.books.open(filename) # ファイルオープン
+                wb = app.books.open('.'+filename) # ファイルオープン
+                # pg = wb.macro('マクロ名')
+                # pg()
+
+
+                # print(os.path.join(pathname,filename))
+                print(filename)
+                print(filename_only)
+                # wb.save(filename = os.path.splitext(filename)[0] + ".xlsx")
+                wb.close()
+                app.quit() # appの終了
+                i += 1
+
+def open():
+    excel = 'sample.xls'
+    subprocess.run(['open',excel])
