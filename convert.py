2575172c9b55e6384e700e25e9fd96a3adb7d53f
reiji-9720 <shino327gh@gmail.com>
2022-12-21 07:25:56 +0900

    完成v2

--- convert.py
+++ convert.py
@@ -7,7 +7,7 @@ from libs import convert_xls_to_xlsx as convert
 # convert.open()
 
 
-convert.simple_check() # カレントディレクトリ内のファイルに対して実行　ディレクトリ下部には影響なし
+convert.simple_check() # カレントディレクトリ内のファイルに対して実行
 
 # convert.convertion(filename='') # filenameのところには変換したいファイル名の相対パスを入力
 
--- libs/convert_xls_to_xlsx.py
+++ libs/convert_xls_to_xlsx.py
@@ -4,7 +4,7 @@ import os
 import xlwings
 
 
-## カレントディレクトリ内での拡張子変更
+## カレントディレクトリ内での拡張子変更 文字データのみ
 def convert_type_current():
     it = glob.glob("*.xls")
     for xls in it:
@@ -12,24 +12,27 @@ def convert_type_current():
         print(xlsx)
         p.save_book_as(file_name='{}'.format(xls), dest_file_name='{}'.format(xlsx))
 
-
+## カレントディレクトリ内での拡張子変更 罫線データ含む (ディレクトリ下部探索)
 def simple_check():
     i = 0
     for pathname,dirname, filenames in os.walk("./"):
         for filename in filenames:
+            print('pathname:'+ pathname)
+            pathname_stack = pathname # filename文字列の保有（そのままpathnameで渡すとpathがうまく行かない）
             if filename.endswith('.xls'): # フィルタ処理 csvのみ処理
-                filename = os.path.join(*pathname,filename) # ファイルの相対パス取得
-                convertion(filename)
+                filenameTo = os.path.join(pathname_stack,filename) # ファイルの相対パス取得
+                print('filename' + filename)
+                convertion(filenameTo)
                 i += 1
 
-
+## 単一ファイルの拡張子変更 罫線データ含む
 def convertion(filename):
     app = xlwings.App() # appの開始
     filename_only = os.path.splitext(filename)[0] # ファイル名拡張子なし
-    wb = app.books.open('.'+filename) # ファイルオープン
+    wb = app.books.open(filename) # ファイルオープン
     print(filename)
     print(filename_only)
-    filename_only = '.'+ filename_only + '.xlsx'
+    filename_only = filename_only + '.xlsx'
     print('filename_only : '+ filename_only)
     wb.save(filename_only)
     wb.close()
