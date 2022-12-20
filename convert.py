19960fa5650f0c00cc7d66c86fd8db799a31b09e
reiji-9720 <shino327gh@gmail.com>
2022-12-21 06:55:50 +0900

    選択肢の追加

--- convert.py
+++ convert.py
@@ -3,11 +3,13 @@ import glob
 from libs import convert_xls_to_xlsx as convert
 
 
-convert.convert_type_current()
+# convert.convert_type_current()
 # convert.open()
 
 
-# convert.simple_check()
+convert.simple_check() # カレントディレクトリ内のファイルに対して実行　ディレクトリ下部には影響なし
+
+# convert.convertion(filename='') # filenameのところには変換したいファイル名の相対パスを入力
 
 
 
